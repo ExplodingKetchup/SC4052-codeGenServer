5b4940cb40f7b9e66c6cc4775babb1cb0c57811b
ExplodingKetchup <96200201+ExplodingKetchup@users.noreply.github.com>
2025-04-06 11:50:55 +0800

    Change request input to json

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from click import prompt
+from flask import Flask, request, Request
 
 import gemini_caller
 
@@ -19,29 +20,45 @@ def load_credentials(filename):
 
 secret = load_credentials("secret.txt")
 
+def get_prompt(received_request: Request) -> str:
+    data = received_request.get_json()
+    if not data or "prompt" not in data:
+        return ""
+    return data["prompt"]
+
 @app.route("/test")
 def test():
     return "hello!"
 
 @app.route("/login", methods=["POST"])
 def login():
-    if request.form.get("username") not in secret:
-        return "WRONG USERNAME!!"
-    if request.form.get("password") != secret[request.form.get("username")]:
-        return "WRONG PASSWORD!!"
+    data = request.get_json()
+    if not data or "username" not in data or data["username"] not in secret:
+        return "WRONG USERNAME!!", 401
+    if not data or "password" not in data or data["password"] != secret[data["username"]]:
+        return "WRONG PASSWORD!!", 401
     return "OK"
 
 @app.route("/import-data", methods=["POST"])
 def import_data():
-    return gemini_caller.codegen_import_data(request.form.get("prompt"))
+    received_prompt = get_prompt(request)
+    if len(received_prompt) == 0:
+        return "Where prompt?", 400
+    return gemini_caller.codegen_import_data(received_prompt)
 
 
 @app.route("/data-processing", methods=["POST"])
 def data_processing():
-    return gemini_caller.codegen_process_data(request.form.get("prompt"))
+    received_prompt = get_prompt(request)
+    if len(received_prompt) == 0:
+        return "Where prompt?", 400
+    return gemini_caller.codegen_process_data(received_prompt)
 
 
 @app.route("/plot-data", methods=["POST"])
 def plot_data():
-    return gemini_caller.codegen_plot_data(request.form.get("prompt"))
+    received_prompt = get_prompt(request)
+    if len(received_prompt) == 0:
+        return "Where prompt?", 400
+    return gemini_caller.codegen_plot_data(received_prompt)
 
